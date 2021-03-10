63b4f77c5ca6227e382af6fbc3aa435ed74e33cb
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2021-03-10 16:39:29 -0500

    Create Multi Module Project. closes #1.master

---
+++ brizel-beauty-spa-cloud-web/src/main/java/com/csgrant/brizelbeautyspacloud/BrizelBeautySpaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.csgrant.brizelbeautyspacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrizelBeautySpaCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrizelBeautySpaCloudApplication.class, args);
+    }
+
+}
---
+++ brizel-beauty-spa-cloud-web/src/test/java/com/csgrant/brizelbeautyspacloud/BrizelBeautySpaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csgrant.brizelbeautyspacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrizelBeautySpaCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
